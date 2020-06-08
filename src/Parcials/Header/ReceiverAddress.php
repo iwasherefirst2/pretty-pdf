6c5adcbd730f1f417c55c44982dea97cb4f39e28
Adam Nielsen <1765602+iwasherefirst2@users.noreply.github.com>
2020-06-08 21:30:01 +0000

    Add addressfield

---
+++ src/Parcials/Header/ReceiverAddress.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace BeautyBill\Parcials\Header;
+
+use BeautyBill\Parcials\ParcialInterface;
+
+class ReceiverAddress implements ParcialInterface
+{
+    /**
+     * Add infobox in the top right corner of invoice
+     * @return Closure
+     */
+    public static function getFunction(): \Closure
+    {
+        return function (array $address) {
+            $this->SetTextColor(0, 0, 0);
+            $this->SetFont('DejaVuSansCondensed', '', 11);
+            $this->SetXY($this->sideMargin, 61);
+            $height = ReceiverAddress::getLineHeight($address, $this->w, $this->sideMargin);
+            foreach ($address as $line) {
+                $this->MultiCell(($this->w)*0.5 - $this->sideMargin, $height, $line, 0, 'L');
+            }
+        };
+    }
+
+    public static function getLineHeight(array $address, int $width, int $margin)
+    {
+        $pdf_dummy = new \BeautyBill\PDF();
+        $pdf_dummy->AddFont('DejaVuSansCondensed', '', 'DejaVuSansCondensed.ttf', true);
+        $pdf_dummy->AddPage();
+        $pdf_dummy->SetFont('DejaVuSansCondensed', '', 11);
+        $pdf_dummy->setY(0);
+
+        // USe dummy_pdf to compute the height so that address fits in evenlope
+
+        if (count($address) >= 7) {
+            $pdf_dummy->SetFont('DejaVuSansCondensed', '', 9);
+        }
+
+        foreach ($address as $line) {
+            $pdf_dummy->MultiCell(($width)*0.5 - $margin, 5, $line, 1, 'L');
+        }
+
+        $num_rows = $pdf_dummy->getY()/5;
+        // Fit text to 25mm envelope space.
+        $height   = ($num_rows > 0) ? 25 / $num_rows : 25;
+
+        return $height;
+    }
+}
---
+++ src/Parcials/Header/ReturnAddress.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BeautyBill\Parcials\Header;
+
+use BeautyBill\Parcials\ParcialInterface;
+
+class ReturnAddress implements ParcialInterface
+{
+    /**
+     * Add infobox in the top right corner of invoice
+     * @return Closure
+     */
+    public static function getFunction(): \Closure
+    {
+        return function (string $address) {
+            $this->SetTextColor(0, 136, 204);
+            $this->SetXY($this->sideMargin, 51);
+            $this->SetFont('DejaVuSansCondensed', '', 7);
+            $this->Cell(100, 10, $address, 0, 1);
+        };
+    }
+}
